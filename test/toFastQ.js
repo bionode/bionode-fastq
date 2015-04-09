563201120cfe4577c45658e3840a6575035b2afd
Bruno Vieira <mail@bmpvieira.com>
2015-04-09 14:46:10 +0100

    Fix Standard style

--- test/toFastQ.js
+++ test/toFastQ.js
@@ -1,28 +1,27 @@
-'use strict';
-var should = require('chai').should();
-var fastq = require('../index');
-
+/* global describe, it */
+'use strict'
+require('chai').should()
+var fastq = require('../index')
 
 describe('toFastQ', function () {
+  this.timeout(4000)
+  var output = []
 
-    this.timeout(4000);
-    var output = [];
+  it('should run without error', function (done) {
+    var applyToEach = function (item) {
+      output.push(item)
+    }
+    var onEnd = function (err) {
+      if (err) {
+        return done(err)
+      }
+      return done()
+    }
+    fastq.toFastQ('test/data/sample.fq', applyToEach, onEnd)
+  })
 
-    it('should run without error', function (done) {
-        var applyToEach = function (item) {
-            output.push(item);
-        };
-        var onEnd = function (err) {
-            if (err) {
-                return done(err);
-            }
-            return done();
-        };
-        fastq.toFastQ('test/data/sample.fq', applyToEach, onEnd);
-    });
-
-    it('should process 100 objects', function (done) {
-        output.length.should.equal(100);
-        done();
-    });
-});
+  it('should process 100 objects', function (done) {
+    output.length.should.equal(100)
+    done()
+  })
+})
