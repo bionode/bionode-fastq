0c3ee96b4b1f2b093bcf42e2e4d95fafbce0d48a
Cole Lyman <cole@colelyman.com>
2017-08-19 15:46:13 -0600

    Updated types from var to const.

--- cli.js
+++ cli.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 
-var fs = require('fs')
-var minimist = require('minimist')
-var fastq = require('./index.js')
+const fs = require('fs')
+const minimist = require('minimist')
+const fastq = require('./index.js')
 
-var argv = minimist(process.argv.slice(2), {
+const argv = minimist(process.argv.slice(2), {
   alias: {
     help: 'h'
   }
@@ -19,9 +19,9 @@ if (argv.help || argv._.length === 0) {
 }
 
 // if there is an output file present, write to the file. Otherwise write to stdout
-var output = argv._[1] ? fs.createWriteStream(argv._[1]) : process.stdout
+const output = argv._[1] ? fs.createWriteStream(argv._[1]) : process.stdout
 
-var fq = fastq.read(argv._[0])
+const fq = fastq.read(argv._[0])
 
 process.stdin.setEncoding('utf8')
 
